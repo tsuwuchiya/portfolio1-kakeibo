7c680a34e457f8c296b66a8d601433f32f4b2e42
tsuwuchiya <tsuwuchiya@gmail.com>
2022-08-02 15:51:07 +0900

    amplify

---
+++ kakeibo/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,19 @@
+export type AmplifyDependentResourcesAttributes = {
+    "api": {
+        "kakeiboapi": {
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
+    },
+    "auth": {
+        "kakeibo21f6a44b": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    }
+}
